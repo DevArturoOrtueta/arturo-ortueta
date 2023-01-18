4b36deb2442d87006d65eb3114ca6e1e941e735c
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-01-17 22:09:36 -0500

    Add Header component and styles

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.css'
+import {Header} from '../src/components/header/Header'
+
+export const App = () => {
+  return (
+    <>
+    <Header/>
+    </>
+  )
+}

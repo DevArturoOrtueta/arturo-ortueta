9bfa4dd5da51f9ccc4d4bb7aaae5429b22bc7ddf
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-01-22 07:50:35 -0500

    Update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,10 +3,18 @@ import './header.css'
 
 export const Header = () => {
 
+    /*=============== TChange Background ===============*/
+
+    window.addEventListener("scroll", function (){
+        const header = document.querySelector(".header");
+        if(this.scrollY >= 80) header.classList.add("scroll-header");
+        else header.classList.remove("scroll-header")
+    })
+
     /*=============== Toggle Menu ===============*/
 
     const [Toggle, showMenu] = useState(false);
-
+    const [activeNav, setActiveNav] = useState("#home");
 
   return (
     <header className="header">
@@ -16,22 +24,22 @@ export const Header = () => {
             <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
                 <ul className="nav__list grid">
                     <li className="nav__item">
-                        <a href="#home" className="nav__link active-link">
+                        <a href="#home" onClick={()=> setActiveNav('#home')} className={activeNav === "#home" ? "nav__link active-link" : "nav__link"}>
                             <i className="uil uil-estate nav__icon"></i>Home
                         </a>
                     </li>
                     <li className="nav__item">
-                        <a href="#about" className="nav__link">
+                        <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav === "#about" ? "nav__link active-link" : "nav__link"}>
                             <i className="uil uil-user nav__icon"></i>About
                         </a>
                     </li>
                     <li className="nav__item">
-                        <a href="#skills" className="nav__link">
+                        <a href="#skills" onClick={()=> setActiveNav('#skills')} className={activeNav === "#skills" ? "nav__link active-link" : "nav__link"}>
                             <i className="uil uil-file nav__icon"></i>Skills
                         </a>
                     </li>
                     <li className="nav__item">
-                        <a href="#services" className="nav__link">
+                        <a href="#services" onClick={()=> setActiveNav('#services')} className={activeNav === "#services" ? "nav__link active-link" : "nav__link"}>
                             <i className="uil uil-briefcase-alt nav__icon"></i>Services
                         </a>
                     </li>

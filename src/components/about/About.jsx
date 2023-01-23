607f1e692168ad6a990ae2a33f68d8f82b15c678
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-01-22 20:55:00 -0500

    Update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './about.css'
 import AboutImg from '../../assets/profile.jpg';
-import CV from '../../assets/John-Cv.pdf';
+import CV from '../../assets/CV - Arturo Ortueta.pdf';
 import Info from './Info';
 
 const About = () => {

3c383538f00748505f9f2da8e15ee69f0b2414b7
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-04-19 16:31:38 -0500

    Update

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -4,7 +4,7 @@ const Data = () => {
     return (
         <div className="home__data">
             <h1 className="home__title">
-                Arturo Ortueta DEV page
+                Arturo Ortueta
                 <svg
                     width="36"
                     height="36"

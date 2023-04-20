dceec512bb817868708ece24270e01dd9f782cb8
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-04-20 15:48:23 -0500

    Add a card jsx

---
+++ src/components/projects/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Card.css'
+
+export const Card = () => {
+    return (
+        <div className="card__container container grid">
+            <div className="card__content">
+                <div>
+                    
+                    <h3 className="card__title">Product <br /> Designer</h3>
+                </div>
+            </div>
+        </div>
+    )
+}

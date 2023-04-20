e05f49199d09fa3ce811a171ce5b5cacc6b9ab1d
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-04-20 16:03:07 -0500

    Update

--- src/components/projects/Card.jsx
+++ src/components/projects/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Card.css'
 
-export const Card = () => {
+export const Card = ({data}) => {
     return (
         <div className="card__container container grid">
             <div className="card__content">
